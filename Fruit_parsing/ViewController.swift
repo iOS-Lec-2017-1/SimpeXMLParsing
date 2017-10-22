a8ad4297f27121a39c028f3fc8b656f4ef5b89e2
김종현 <jhkim@dit.ac.kr>
2017-10-22 15:50:23 +0900

    1st commit

--- Fruit_parsing/ViewController.swift
+++ Fruit_parsing/ViewController.swift
@@ -8,18 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    var item:[String:String] = [:]
+    var elements:[[String:String]] = []
+    var currentElement = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //let strURL = "http://api.androidhive.info/pizza/?format=xml"
+        
+        if let path = Bundle.main.url(forResource: "Fruit", withExtension: "xml") {
+        
+            if let parser = XMLParser(contentsOf: path) {
+                parser.delegate = self
+                //print(path)
+                
+                if parser.parse() {
+                    print("parsing success!")
+                    print(elements)
+                } else {
+                    print("parsing failed!!")
+                }
+            }
+        } else {
+            print("xml file not found!!")
+        }
+ 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        
+        print("elementName = \(elementName)")
+        
+        currentElement = elementName
+        
+        if elementName == "item" {
+            item = [:]
+        } else if elementName == "elements" {
+            elements = []
+        }
     }
-
-
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        
+        let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        print("data =  \(data)")
+        if !data.isEmpty {
+            item[currentElement] = string
+       
+            print("currentElement = \(currentElement)")
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName == "item" {
+            elements.append(item)
+            print("item = \(item)")
+            print("elements = \(elements)")
+        }
+    }
+    
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("failure error: ", parseError)
+    }
 }
 
